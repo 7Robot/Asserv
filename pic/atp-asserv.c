c0489eae54e5984dec88e80f6cac689e8e26e424
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-05-29 15:12:02 +0200

    maj automatique de la sémantique

--- pic/atp-asserv.c
+++ pic/atp-asserv.c
@@ -1,4 +1,4 @@
-// Generated from version 1305110948 of semantic
+// Generated from version 1305291511 of semantic
 
 #include "atp.h"
 
@@ -63,17 +63,152 @@ void SendTest(unsigned char B, unsigned int H, unsigned long int I, char b, int 
     SendBytes(bytes, 28);
 }
 
-// You should redefine this function
-__attribute__((weak)) void OnAvanceUnPeuAuCasOu() {}
+void SendX(float x) {
+    char bytes[] = {
+        129,
+        32,
+        36,
+        ((char*)&x)[0],
+        ((char*)&x)[1],
+        ((char*)&x)[2],
+        ((char*)&x)[3],
+        128
+    };
+    SendBytes(bytes, 8);
+}
+
+void SendY(float y) {
+    char bytes[] = {
+        129,
+        35,
+        36,
+        ((char*)&y)[0],
+        ((char*)&y)[1],
+        ((char*)&y)[2],
+        ((char*)&y)[3],
+        128
+    };
+    SendBytes(bytes, 8);
+}
 
 // You should redefine this function
-__attribute__((weak)) void OnGobeletDeux() {}
+__attribute__((weak)) void OnDist(float dist, float vMax, float aMax) {}
 
 // You should redefine this function
-__attribute__((weak)) void OnGobeletUn() {}
+__attribute__((weak)) void OnDistFree(float dist) {}
 
 // You should redefine this function
-__attribute__((weak)) void OnMaison() {}
+__attribute__((weak)) void OnDistRot(float dist, float rot, float vDistMax, float aDistMax, float vRotMax, float aRotMax) {}
+
+void SendDone() {
+    char bytes[] = {
+        129,
+        2,
+        128
+    };
+    SendBytes(bytes, 3);
+}
+
+// You should redefine this function
+__attribute__((weak)) void OnGetPos() {}
+
+// You should redefine this function
+__attribute__((weak)) void OnGetTheta() {}
+
+// You should redefine this function
+__attribute__((weak)) void OnGetX() {}
+
+// You should redefine this function
+__attribute__((weak)) void OnGetY() {}
+
+// You should redefine this function
+__attribute__((weak)) void OnOmega(float omega, float aMax, float dMax) {}
+
+void SendPos(float x, float y, float theta) {
+    char bytes[] = {
+        129,
+        40,
+        36,
+        ((char*)&x)[0],
+        ((char*)&x)[1],
+        ((char*)&x)[2],
+        ((char*)&x)[3],
+        36,
+        ((char*)&y)[0],
+        ((char*)&y)[1],
+        ((char*)&y)[2],
+        ((char*)&y)[3],
+        36,
+        ((char*)&theta)[0],
+        ((char*)&theta)[1],
+        ((char*)&theta)[2],
+        ((char*)&theta)[3],
+        128
+    };
+    SendBytes(bytes, 18);
+}
+
+// You should redefine this function
+__attribute__((weak)) void OnReachTheta(float theta, float vMax, float aMax) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnReachX(float x, float vMax, float aMax) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnReachY(float y, float vMax, float aMax) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnRot(float rot, float vMax, float aMax) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnRotFree(float rot) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSetEpsilons(float dist, float speed, float theta, float omega) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSetSpacing(float spacing) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSetTheta(float theta) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSetTicByMeter(unsigned long int tic_by_meter) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSetX(float x) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSetY(float y) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSpeed(float speed, float aMax, float dMax) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSpeedFree(float speed) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnSpeedOmega(float speed, float omega, float aDistMax, float dDistMax, float aRotMax, float dRotMax) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnStep(unsigned long int period, unsigned long int ticsG, unsigned long int ticsD, float consignG, float consignD) {}
+
+// You should redefine this function
+__attribute__((weak)) void OnStop() {}
+
+void SendTheta(float theta) {
+    char bytes[] = {
+        129,
+        38,
+        36,
+        ((char*)&theta)[0],
+        ((char*)&theta)[1],
+        ((char*)&theta)[2],
+        ((char*)&theta)[3],
+        128
+    };
+    SendBytes(bytes, 8);
+}
 
 
 int AtpDecode(int id,
@@ -93,20 +228,100 @@ int AtpDecode(int id,
         OnTest(ucharv[0], ushortv[0], uintv[0], charv[0], shortv[0], intv[0], floatv[0]);
         return 1;
     }
-    if (id == 4) {
-        OnAvanceUnPeuAuCasOu();
+    if (id == 12) {
+        OnDist(floatv[0], floatv[1], floatv[2]);
         return 1;
     }
-    if (id == 2) {
-        OnGobeletDeux();
+    if (id == 14) {
+        OnDistFree(floatv[0]);
+        return 1;
+    }
+    if (id == 16) {
+        OnDistRot(floatv[0], floatv[1], floatv[2], floatv[3], floatv[4], floatv[5]);
+        return 1;
+    }
+    if (id == 39) {
+        OnGetPos();
+        return 1;
+    }
+    if (id == 36) {
+        OnGetTheta();
+        return 1;
+    }
+    if (id == 30) {
+        OnGetX();
+        return 1;
+    }
+    if (id == 33) {
+        OnGetY();
+        return 1;
+    }
+    if (id == 22) {
+        OnOmega(floatv[0], floatv[1], floatv[2]);
+        return 1;
+    }
+    if (id == 19) {
+        OnReachTheta(floatv[0], floatv[1], floatv[2]);
+        return 1;
+    }
+    if (id == 17) {
+        OnReachX(floatv[0], floatv[1], floatv[2]);
+        return 1;
+    }
+    if (id == 18) {
+        OnReachY(floatv[0], floatv[1], floatv[2]);
+        return 1;
+    }
+    if (id == 13) {
+        OnRot(floatv[0], floatv[1], floatv[2]);
+        return 1;
+    }
+    if (id == 15) {
+        OnRotFree(floatv[0]);
+        return 1;
+    }
+    if (id == 11) {
+        OnSetEpsilons(floatv[0], floatv[1], floatv[2], floatv[3]);
+        return 1;
+    }
+    if (id == 42) {
+        OnSetSpacing(floatv[0]);
+        return 1;
+    }
+    if (id == 37) {
+        OnSetTheta(floatv[0]);
+        return 1;
+    }
+    if (id == 41) {
+        OnSetTicByMeter(uintv[0]);
+        return 1;
+    }
+    if (id == 31) {
+        OnSetX(floatv[0]);
+        return 1;
+    }
+    if (id == 34) {
+        OnSetY(floatv[0]);
+        return 1;
+    }
+    if (id == 20) {
+        OnSpeed(floatv[0], floatv[1], floatv[2]);
+        return 1;
+    }
+    if (id == 21) {
+        OnSpeedFree(floatv[0]);
+        return 1;
+    }
+    if (id == 23) {
+        OnSpeedOmega(floatv[0], floatv[1], floatv[2], floatv[3], floatv[4], floatv[5]);
+        return 1;
+    }
+    if (id == 10) {
+        OnStep(uintv[0], uintv[1], uintv[2], floatv[0], floatv[1]);
         return 1;
     }
     if (id == 1) {
-        OnGobeletUn();
-        return 1;
-    }
-    if (id == 3) {
-        OnMaison();
+        OnStop();
         return 1;
     }
     return 0;
--- pic/atp-asserv.h
+++ pic/atp-asserv.h
@@ -1,4 +1,4 @@
-// Generated from version 1305110948 of semantic
+// Generated from version 1305291511 of semantic
 
 #ifndef _ASSERV_H_
 #define _ASSERV_H_
@@ -17,13 +17,63 @@ void OnTest(unsigned char B, unsigned int H, unsigned long int I, char b, int h,
 
 void SendTest(unsigned char B, unsigned int H, unsigned long int I, char b, int h, long int i, float f);
 
-void OnAvanceUnPeuAuCasOu();
+void SendX(float x);
 
-void OnGobeletDeux();
+void SendY(float y);
 
-void OnGobeletUn();
+void OnDist(float dist, float vMax, float aMax);
 
-void OnMaison();
+void OnDistFree(float dist);
+
+void OnDistRot(float dist, float rot, float vDistMax, float aDistMax, float vRotMax, float aRotMax);
+
+void SendDone();
+
+void OnGetPos();
+
+void OnGetTheta();
+
+void OnGetX();
+
+void OnGetY();
+
+void OnOmega(float omega, float aMax, float dMax);
+
+void SendPos(float x, float y, float theta);
+
+void OnReachTheta(float theta, float vMax, float aMax);
+
+void OnReachX(float x, float vMax, float aMax);
+
+void OnReachY(float y, float vMax, float aMax);
+
+void OnRot(float rot, float vMax, float aMax);
+
+void OnRotFree(float rot);
+
+void OnSetEpsilons(float dist, float speed, float theta, float omega);
+
+void OnSetSpacing(float spacing);
+
+void OnSetTheta(float theta);
+
+void OnSetTicByMeter(unsigned long int tic_by_meter);
+
+void OnSetX(float x);
+
+void OnSetY(float y);
+
+void OnSpeed(float speed, float aMax, float dMax);
+
+void OnSpeedFree(float speed);
+
+void OnSpeedOmega(float speed, float omega, float aDistMax, float dDistMax, float aRotMax, float dRotMax);
+
+void OnStep(unsigned long int period, unsigned long int ticsG, unsigned long int ticsD, float consignG, float consignD);
+
+void OnStop();
+
+void SendTheta(float theta);
 
 
 int AtpDecode(int id,
